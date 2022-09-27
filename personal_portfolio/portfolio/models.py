befa4861870d12548d59e71645807b4d4be5b48d
TrickyTenshi <110241219+TrickyTenshi@users.noreply.github.com>
2022-09-27 16:26:08 +0300

    Add files via upload

---
+++ personal_portfolio/portfolio/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# задаем максимальное количество отображение символов на сайте
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
